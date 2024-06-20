1ffc6d338f68ca61571e07e80907b09ea6f47232
padidas <paddybaber@gmail.com>
2024-06-20 21:26:14 +0200

    add shadcn ui

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/MainHeading.vue'
+import { useColorMode } from '@vueuse/core'
+const mode = useColorMode()
+
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <HelloWorld msg="You did it!" />
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
---
+++ src/components/MainHeading.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-3xl font-semibold">Schuffle</h1>
+</template>
