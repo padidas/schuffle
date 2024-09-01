b53f629dfd040254c497308b96378c91eb3bcabe
padidas <paddybaber@gmail.com>
2024-09-01 18:30:53 +0200

    remove validate function

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,16 +1,8 @@
 import { describe, it, expect } from 'vitest'
-import { isValidNumberString } from '@/lib/validateLevelInput'
 
 describe('HelloWorld', () => {
   it('validates level correctly', () => {
-    expect(isValidNumberString(1)).toBeTruthy()
-    expect(isValidNumberString(2)).toBeTruthy()
-    expect(isValidNumberString(3)).toBeTruthy()
-    expect(isValidNumberString(4)).toBeTruthy()
-    expect(isValidNumberString(5)).toBeTruthy()
-    expect(isValidNumberString(22)).toBeFalsy()
-    expect(isValidNumberString(-1)).toBeFalsy()
-    expect(isValidNumberString(0)).toBeFalsy()
-    expect(isValidNumberString(-5)).toBeFalsy()
+    expect(true).toBeTruthy()
+    expect(1).toBeTruthy()
   })
 })
