a9c6825bf33e2fd4025f80ecd8a7e48111f6e445
padidas <paddybaber@gmail.com>
2024-09-13 19:55:21 +0200

    go back to slider as new student level input

--- src/components/StudentInput.vue
+++ src/components/StudentInput.vue
@@ -6,9 +6,8 @@ import { toTypedSchema } from '@vee-validate/zod'
 import { z } from 'zod'
 import { useForm } from 'vee-validate'
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Slider } from './ui/slider'
 import { Check, Loader } from 'lucide-vue-next'
-import { Label } from './ui/label'
-import LevelInput from './LevelInput.vue'
 
 defineProps<{
   isLoading: boolean
@@ -18,20 +17,17 @@ const emit = defineEmits<{
   (e: 'addStudent', name: string, lvl: number): void
 }>()
 
-const DEFAULT_LEVEL = 2
+const DEFAULT_LEVEL = [2]
 
 const formSchema = toTypedSchema(
   z.object({
     name: z.string().min(2).max(30),
-    level: z.number().min(1).max(3).default(DEFAULT_LEVEL)
+    level: z.array(z.number().min(1).max(5)).default(DEFAULT_LEVEL)
   })
 )
 
-const { errors, handleSubmit, resetForm, setFieldValue } = useForm({
-  validationSchema: formSchema,
-  initialValues: {
-    level: DEFAULT_LEVEL
-  }
+const { errors, handleSubmit, resetForm } = useForm({
+  validationSchema: formSchema
 })
 
 const onSubmit = handleSubmit((values) => {
@@ -44,7 +40,7 @@ const onSubmit = handleSubmit((values) => {
     return
   }
   if (values.name && values.level) {
-    emit('addStudent', values.name, values.level)
+    emit('addStudent', values.name, values.level[0])
     resetInput()
   }
 })
@@ -52,32 +48,33 @@ const onSubmit = handleSubmit((values) => {
 function resetInput() {
   resetForm()
 }
-
-function getPlaceholder() {
-  const names = ['Hodor', 'Jon', 'Arya', 'Tyrion', 'Bran', 'Jaime']
-  const index = Math.floor(Math.random() * names.length)
-  return names[index]
-}
 </script>
 
 <template>
-  <form @submit="onSubmit" class="flex gap-4 mb-4 border p-4 rounded-md">
-    <FormField v-slot="{ componentField }" name="name">
-      <FormItem class="flex flex-col flex-[2]">
-        <FormLabel>Name</FormLabel>
-        <FormControl>
-          <Input type="text" :placeholder="getPlaceholder()" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-    <LevelInput @set-field-value="setFieldValue" />
-    <div class="flex flex-col justify-between">
-      <Label></Label>
-      <Button type="submit">
-        <Loader v-if="isLoading" />
-        <Check v-else />
-      </Button>
+  <form @submit="onSubmit" class="flex flex-col gap-4 mb-4 border-b pb-4">
+    <div class="flex gap-4">
+      <FormField v-slot="{ componentField }" name="name">
+        <FormItem class="flex flex-col flex-[3]">
+          <FormLabel>Name</FormLabel>
+          <FormControl>
+            <Input type="text" placeholder="Hodor" v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <FormField v-slot="{ componentField, value }" name="level">
+        <FormItem class="flex flex-col flex-1">
+          <FormLabel>Level: {{ value?.[0] }}</FormLabel>
+          <FormControl class="flex flex-grow">
+            <Slider :default-value="DEFAULT_LEVEL" :min="1" :max="3" v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
     </div>
+    <Button type="submit">
+      <Loader v-if="isLoading" />
+      <Check v-else />
+    </Button>
   </form>
 </template>
